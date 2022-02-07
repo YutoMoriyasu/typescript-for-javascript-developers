714ef1c6f4cf3b252c606b691ae9e50e08ea8f9f
Yuto Moriyasu <y.moriyasu@icloud.com>
2022-02-07 19:14:32 +0900

    any型までの学習

---
+++ src/array.ts
@@ -0,0 +1,11 @@
+export{};
+
+let numbers: number[] = [1, 2, 3];
+let numbers2: Array<number> = [1, 2, 3];
+console.log({numbers});
+console.log({numbers2});
+
+let strings2: string[] = ['tokyo', 'osaka', 'kyoto'];
+console.log({strings2});
+
+
